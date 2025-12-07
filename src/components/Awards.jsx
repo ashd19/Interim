16f6e059744d697614348e672ea495f6a461243b
Ashton <ashtondsouza192@gmail.com>
2025-12-07 23:34:06 +0530

    Badge sub component done

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -1,9 +1,10 @@
 import { CircleStop } from "lucide-react";
+import Badge from "../subcomponents/Badge";
 
 export default function Awards() {
   return (
-    <div className="w-full h-screen  relative overflow-hidden flex flex-col p- bg-[#1f3831] rounded-xl">
-      <div className="flex absolute top-0 left-0 translate-x-110 translate-y-25 ">
+    <div className="w-full h-screen  relative overflow-hidden flex flex-col  bg-[#1f3831] rounded-xl">
+      <div className="flex absolute top-0 left-0 translate-x-110 translate-y-25  ">
         <div>
           <CircleStop
             height={12}
@@ -16,9 +17,11 @@ export default function Awards() {
           RECOGNITIONS WE GOT
         </h1>
       </div>
-      <h1 className=" ml-85 mt-5 text-[14vw] tracking-tighter text-[#fcfdee] font-semibold">
+      <h1 className=" ml-85 mt-5 mb-10   text-[14vw] tracking-tighter text-[#fcfdee] font-semibold">
         Awards
       </h1>
+
+      <Badge />
     </div>
   );
 }
