6f8d4bbaf2f0d7b4784c1c9106003599480f4c66
Ashton <ashtondsouza192@gmail.com>
2025-12-07 22:52:38 +0530

    recent changes, awards setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Client from "./components/Client";
 import Footer from "./components/Footer";
 import Pacakages from "./components/Pacakages";
+import Awards from "./components/Awards";
 const App = () => {
   return (
     <div className="bg-white p-10 ">
+      <Awards />
       <Pacakages />
       <Client />
       <Footer />
--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -1,14 +1,17 @@
+
+import { CircleStop } from "lucide-react";
+
 export default function Awards() {
     return (
-          <div className="w-full h-screen relative overflow-hidden">
-            <div className="flex absolute top-0 left-0 translate-x-168 translate-y-20 ">
+        <div className="w-full h-screen relative overflow-hidden bg-[#1f3831] rounded-xl">
+            <div className="flex absolute top-0 left-0 translate-x-90 translate-y-20 ">
                 <div>
-                    <CircleStop height={12} width={12} className="mt-0.5 mr-1" />
+                    <CircleStop height={12} width={12} color="#f2ac83" className="mt-0.5 mr-1" />
                 </div>
-                <h1 className="text-[#1d332c] font-light text-sm">RECOGNITIONS WE GOT</h1>
+                <h1 className="text-[#f2ac83] font-light text-sm">RECOGNITIONS WE GOT</h1>
 
             </div>
-            <h1 className="absolute top-0 left-0 translate-x-65  text-[14vw] tracking-tighter text-[#1d332c] font-semibold">Awards</h1>
+            <h1 className="absolute top-0 left-0 translate-x-65  text-[14vw] tracking-tighter text-[#fcfdee] font-semibold">Awards</h1>
         </div>
     );
 }   
