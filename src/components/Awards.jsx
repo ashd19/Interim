456c9d2f9f0e4efe07ecafeff1fe1ef896113c82
Ashton <ashtondsouza192@gmail.com>
2025-12-07 22:34:37 +0530

    position changes

---
+++ src/components/Awards.jsx
@@ -0,0 +1,14 @@
+export default function Awards() {
+    return (
+          <div className="w-full h-screen relative overflow-hidden">
+            <div className="flex absolute top-0 left-0 translate-x-168 translate-y-20 ">
+                <div>
+                    <CircleStop height={12} width={12} className="mt-0.5 mr-1" />
+                </div>
+                <h1 className="text-[#1d332c] font-light text-sm">RECOGNITIONS WE GOT</h1>
+
+            </div>
+            <h1 className="absolute top-0 left-0 translate-x-65  text-[14vw] tracking-tighter text-[#1d332c] font-semibold">Awards</h1>
+        </div>
+    );
+}   
