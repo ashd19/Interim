b4f77f717375a2de4f6eb2789aa883881b5d1c70
Ashton <ashtondsouza192@gmail.com>
2025-12-07 22:22:26 +0530

    Packages done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Client from "./components/Client";
 import Footer from "./components/Footer";
+import Pacakages from "./components/Pacakages";
 const App = () => {
   return (
     <div className="bg-white p-10 ">
+      <Pacakages />
       <Client />
       <Footer />
     </div>
