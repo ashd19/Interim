628ec902b670ab0f37b315e92d775985bfdf87bd
Ashton <ashtondsouza192@gmail.com>
2025-12-07 23:19:28 +0530

    used flex containers instead of absoulte positioning

--- src/components/Awards.jsx
+++ src/components/Awards.jsx
@@ -1,17 +1,24 @@
-
 import { CircleStop } from "lucide-react";
 
 export default function Awards() {
-    return (
-        <div className="w-full h-screen relative overflow-hidden bg-[#1f3831] rounded-xl">
-            <div className="flex absolute top-0 left-0 translate-x-90 translate-y-20 ">
-                <div>
-                    <CircleStop height={12} width={12} color="#f2ac83" className="mt-0.5 mr-1" />
-                </div>
-                <h1 className="text-[#f2ac83] font-light text-sm">RECOGNITIONS WE GOT</h1>
-
-            </div>
-            <h1 className="absolute top-0 left-0 translate-x-65  text-[14vw] tracking-tighter text-[#fcfdee] font-semibold">Awards</h1>
+  return (
+    <div className="w-full h-screen  relative overflow-hidden flex flex-col p- bg-[#1f3831] rounded-xl">
+      <div className="flex absolute top-0 left-0 translate-x-110 translate-y-25 ">
+        <div>
+          <CircleStop
+            height={12}
+            width={12}
+            color="#f2ac83"
+            className="mt-0.5 mr-1"
+          />
         </div>
-    );
-}   
+        <h1 className="text-[#f2ac83] font-light text-sm">
+          RECOGNITIONS WE GOT
+        </h1>
+      </div>
+      <h1 className=" ml-85 mt-5 text-[14vw] tracking-tighter text-[#fcfdee] font-semibold">
+        Awards
+      </h1>
+    </div>
+  );
+}
