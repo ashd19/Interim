14b0ae48b41ee6567d5b56c9f7e45f1e193cf3d4
Ashton <ashtondsouza192@gmail.com>
2025-12-10 15:53:43 +0530

    buttons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ import ProjectCard from "../subcomponents/ProjectCard";
 export default function Projects() {
     return (
         <>
-            <div className="w-full min-h-screen relative  px-4 py-4  overflow-hidden">
+            <div className="w-full min-h-screen relative  px-4 py-4  mb-15 overflow-hidden">
 
                 <h1 className="text-services interServices text-center  tracking-tight text-[#1e3730]">Projects</h1>
                 <div className="flex gap-1 absolute top-0 left-1/2 -translate-x-65 translate-y-32 items-center  justify-center">
@@ -13,7 +13,7 @@ export default function Projects() {
 
                 </div>
                 <div className="flex flex-col gap-3">
-                    <ProjectCard firstButton="RESIDENTIAL" secondButton="SINGLE HOME" imgurl="" heading={<>Costal Harmony <br />Home </>} text="The Coastal Harmony Home project was a comprehensive renovation of a beachside house, transforming it into a serene and stylish retreat that reflects the client's love for the ocean and nature." />
+                    <ProjectCard firstButton="RESIDENTIAL" secondButton="SINGLE HOME" imgurl="https://framerusercontent.com/images/YROLmfdQa2zqlwGcAWrlPhNRVk.jpg?scale-down-to=1024" heading={<>Costal Harmony <br />Home </>} text="The Coastal Harmony Home project was a comprehensive renovation of a beachside house, transforming it into a serene and stylish retreat that reflects the client's love for the ocean and nature." />
                 </div>
 
             </div>
--- src/subcomponents/ProjectCard.jsx
+++ src/subcomponents/ProjectCard.jsx
@@ -1,8 +1,11 @@
 export default function ProjectCard({firstButton, secondButton, imgurl , heading ,text}) {
     return (
         <>
-            <div className="w-[900px] h-[400px] rounded-xl bg-[#f6f7e9]">
-        
+            <div className="w-[1200px] h-[420px] rounded-xl p-5 bg-[#f6f7e9] flex ">
+            <div className="flex gap-2 items-start ">
+                <button className="px-1 py-1 uppercase rounded-lg h-[30px] w-[100px] border border-[#1e3730]   text-[#1e3730] inter-nav text-[0.8rem] ">{secondButton}</button>
+                <button className="px-1 py-1 uppercase rounded-lg h-[30px] w-[100px] border border-[#1e3730]   text-[#1e3730] inter-nav text-[0.8rem] ">{firstButton}</button>
+            </div>
             </div>
         </>
     )
