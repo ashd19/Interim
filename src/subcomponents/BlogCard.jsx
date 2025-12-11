b850a7dc456221f055b107589ade884f9ad5b1b9
Ashton <ashtondsouza192@gmail.com>
2025-12-11 12:33:25 +0530

    tag banner done

--- src/subcomponents/BlogCard.jsx
+++ src/subcomponents/BlogCard.jsx
@@ -8,24 +8,15 @@ export default function BlogCard({ imgurl, tag, date, timeToRead, heading, }) {
                         <div className="w-[260px] h-[260px] mt-4 overflow-hidden rounded-xl">
                             <img src={imgurl} alt="" className="w-full h-full object-cover   rounded-xl transition-transform duration-700 ease-in group-hover:scale-110 " />
                         </div>
+                <div className="flex flex-col justify-start">
+                    <div className="flex">
+                        <div className="rounded-full w-fit px-3 py-1 h-[25px] bg-[#f3ad84] mt-7 ml-1 inter-nav text-[#1d332c] text-[0.7rem] font-semibold uppercase">{tag}</div>
+                    </div>
+                </div>
                     </div>
                 </div>
             </a>
 
-            {/* 
-            // ZOOM EFFECT VERSION (Requested Revision)
-            <div className="bg-[#fefff2] rounded-xl px-3   w-[700px] h-[290px] group cursor-pointer overflow-hidden mt-10">
-                <div className="flex gap-2">
-                    <div className="w-[280px] h-[260px] mt-4 overflow-hidden rounded-xl">
-                        <img 
-                            src={imgurl} 
-                            alt="" 
-                            className="w-full h-full object-cover transition-transform duration-500 ease-in-out group-hover:scale-110" 
-                        />
-                    </div>
-                </div>
-            </div> 
-            */}
         </>
     )
 }
